769c995fcdce63819832d42274380e407a19f171
Antara055 <antara55.dgp10.1434@gmail.com>
2021-12-04 20:22:58 +0530

    Added:function

--- empWage.js
+++ empWage.js
@@ -1,32 +1,25 @@
-//UC1
+//UC3
 {
-    const IS_ABSENT=0;
-    let empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck==IS_ABSENT){
-        console.log("#UC1-Employee is Absent");
-    } else{
-        console.log("#UC1-Employee is Present");  
+    const IS_PART_TIME=1;
+    const IS_FULL_TIME=2;
+    const PART_TIME_HOUR=4;
+    const FULL_TIME_HOUR=8;
+    const WAGE_PER_HOUR=20;
+    {
+        function getWorkinhHours(empCh){
+            switch(empCh){
+                case IS_FULL_TIME:
+                    return FULL_TIME_HOUR;
+                case IS_PART_TIME:
+                     return PART_TIME_HOUR;
+                default:
+                    return 0;    
+            }
+        }
+        let empCheck=Math.floor(Math.random()*10)%3;
+        let empHrs=getWorkinhHours(empCheck);
+        let empWage=empHrs*WAGE_PER_HOUR;
+        console.log("Employee wage : "+empWage);
+        }
     }
-}
-//UC2
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOUR=4;
-const FULL_TIME_HOUR=8;
-const WAGE_PER_HOUR=20;
-{
-    let empHrs=0;
-    let empCheck=Math.floor(Math.random()*10)%3;
-    switch(empCheck){
-        case IS_FULL_TIME:
-            empHrs=FULL_TIME_HOUR;
-            break;
-        case IS_PART_TIME:
-            empHrs=PART_TIME_HOUR;
-            break;
-        default:
-            empHrs=0;    
-    }
-    let empWage=empHrs*WAGE_PER_HOUR;
-    console.log("#UC2-Employee wage : "+empWage);
-}
+
