d33953df028204cf0320113af95d1f7d95f60ef2
Antara055 <antara55.dgp10.1434@gmail.com>
2021-12-04 20:59:55 +0530

    Added:for loop

--- empWage.js
+++ empWage.js
@@ -1,10 +1,11 @@
-//UC3
+//UC4
 {
     const IS_PART_TIME=1;
     const IS_FULL_TIME=2;
     const PART_TIME_HOUR=4;
     const FULL_TIME_HOUR=8;
     const WAGE_PER_HOUR=20;
+    const NO_OF_WORKING_DAYS=20;
     {
         function getWorkinhHours(empCh){
             switch(empCh){
@@ -16,10 +17,14 @@
                     return 0;    
             }
         }
+        let totalEmpHours=0;
+        for(let i=0;i<NO_OF_WORKING_DAYS;i++){
         let empCheck=Math.floor(Math.random()*10)%3;
-        let empHrs=getWorkinhHours(empCheck);
-        let empWage=empHrs*WAGE_PER_HOUR;
-        console.log("Employee wage : "+empWage);
+        totalEmpHours=getWorkinhHours(empCheck)+totalEmpHours;
+        }
+        let empWage=totalEmpHours*WAGE_PER_HOUR;
+        
+        console.log("Employee is work for"+totalEmpHours+"hours and total Employee wage is: "+empWage);
         }
     }
 
