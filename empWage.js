f8601812c4dc8de730c91d9689af7f1b4fcf0ded
Antara055 <antara55.dgp10.1434@gmail.com>
2021-12-04 21:12:30 +0530

    Added:while loop

--- empWage.js
+++ empWage.js
@@ -1,4 +1,4 @@
-//UC4
+//UC5
 {
     const IS_PART_TIME=1;
     const IS_FULL_TIME=2;
@@ -6,6 +6,7 @@
     const FULL_TIME_HOUR=8;
     const WAGE_PER_HOUR=20;
     const NO_OF_WORKING_DAYS=20;
+    const MAX_HOURS_IN_MONTH=160;
     {
         function getWorkinhHours(empCh){
             switch(empCh){
@@ -18,13 +19,15 @@
             }
         }
         let totalEmpHours=0;
-        for(let i=0;i<NO_OF_WORKING_DAYS;i++){
+        let workingDays=0;
+        while(totalEmpHours<MAX_HOURS_IN_MONTH && workingDays<NO_OF_WORKING_DAYS){
         let empCheck=Math.floor(Math.random()*10)%3;
         totalEmpHours=getWorkinhHours(empCheck)+totalEmpHours;
+        workingDays++;
         }
         let empWage=totalEmpHours*WAGE_PER_HOUR;
         
-        console.log("Employee is work for"+totalEmpHours+"hours and total Employee wage is: "+empWage);
+        console.log("Employee is work for"+totalEmpHours+"hours,"+workingDays+"days and total Employee wage is: "+empWage);
         }
     }
 
